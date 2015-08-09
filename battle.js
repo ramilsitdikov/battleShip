19b6a74335dba96b472c296776c91ade731664f6
Ramil <sitdikov.ramil@gmail.com>
2015-08-09 14:27:55 +0300

    Added animation, pictures

--- battle.js
+++ battle.js
@@ -12,15 +12,34 @@ window.onload = function(width, height) {
                 'ssooooooso'],
         player = document.getElementById('player_field'),
         computer = document.getElementById('computer_field'),
-        output = document.getElementById("output"),
+        output = document.getElementById('output'),
         button = document.getElementById('start');
 
     output.innerHTML = "<p>Конфигурация кораблей: 1 x 4, 2 x 3, 3 x 2, 4 x 1</p>";
 
+    function fire(element) {
+        if (element.className == 'dead' || element.className == 'missed') return false;
+        element.className = element.className == 'ship' ? 'dead' : 'missed';
+        if (document.querySelectorAll('#computer_field .ship').length === 0) {
+          alert('Победа!'); 
+          return false;
+        }
+        if (element.className == 'missed') return true;
+      }
+      function backfire() {
+        for (i = width * height; i > 0; i--) {
+          var targets = document.querySelectorAll('#player_field .ship, #player_field .water');
+          if (targets.length === 0 || fire(targets[Math.floor(Math.random() * targets.length)])) break;
+        }
+        if (document.querySelectorAll('#player_field .ship').length === 0) alert('Вы проиграли. Обновите страницу для новой игры.');
+      }
+
+
     button.onclick = function () {
-      //button.disabled = true;
+      button.disabled = true;
+      //Создание кораблей
       for (var lineNum = 0; lineNum < width; lineNum++) {
-        var shipLine = prompt("Расположение кораблей на линии " + lineNum, "ossoooosoo");
+        var shipLine = prompt("Расположение кораблей на линии #" + lineNum, "sooooooooo");
         playerMap.push(shipLine);
         if (playerMap[lineNum] != undefined) {
           output.innerHTML += "<p class='ships-line'>" + playerMap[lineNum] + "</p>";
@@ -33,31 +52,15 @@ window.onload = function(width, height) {
           div1.id = i+'_'+j;
           div1.className = playerMap[i][j];
           if (playerMap[i][j] == 's'){
-            div1.className = 's';
+            div1.className = 'ship';
           } else {
-            div1.className = 'w';
+            div1.className = 'water';
           }
           player.appendChild(div1);
           div2 = document.createElement('div');
-          div2.className = computerMap[i][j] == 's' ? 's' : 'w';
+          div2.className = computerMap[i][j] == 's' ? 'ship' : 'water';
           div2.onclick = function () { if (fire(this)) backfire(); };
           computer.appendChild(div2);
       }
     };
-      function fire(el) {
-        if (el.className == 'd' || el.className == 'm') return false;
-        el.className = el.className == 's' ? 'd' : 'm';
-        if (document.querySelectorAll('#computer_field .s').length === 0) {
-          alert('Victory!'); 
-          return false;
-        }
-        if (el.className == 'm') return true;
-      }
-      function backfire() {
-        for (i = width * height; i > 0; i--) {
-          var targets = document.querySelectorAll('#player_field .s, #player_field .w');
-          if (targets.length === 0 || fire(targets[Math.floor(Math.random() * targets.length)])) break;
-        }
-        if (document.querySelectorAll('#player_field .s').length === 0) alert('You have lost! Refresh the page to start new game.');
-      }
-    }(10, 10);
+  }(10, 10);
